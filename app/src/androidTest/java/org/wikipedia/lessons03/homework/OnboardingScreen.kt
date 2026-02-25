cc8394b056bcb05a874c497eea8c939497ae5810
anlagunov <an.lagunov@korona.net>
2026-02-25 16:36:50 +0300

    Add onboarding screen elements for lesson 3

---
+++ app/src/androidTest/java/org/wikipedia/lessons03/homework/OnboardingScreen.kt
@@ -0,0 +1,61 @@
+package org.wikipedia.lessons03.homework
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.tabs.TabLayout
+import org.wikipedia.R
+import org.wikipedia.views.AppTextView
+
+val zeroPageLanguageOptionLabel = listOf(
+    AppTextView::class.java,
+    R.id.option_label,
+    null
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_done_button,
+    R.string.onboarding_get_started
+)
+
+val pageIndicator = listOf(
+    TabLayout::class.java,
+    R.id.view_onboarding_page_indicator,
+    null
+)
+
+val addLanguagesButton = listOf(
+    MaterialButton::class.java,
+    R.id.addLanguageButton,
+    R.string.onboarding_multilingual_add_language_text
+)
+
+val primaryText = listOf(
+    AppTextView::class.java,
+    R.id.primaryTextView,
+    R.string.onboarding_welcome_title_v2
+)
+
+val zeroPageSecondaryText = listOf(
+    AppTextView::class.java,
+    R.id.secondaryTextView,
+    R.string.onboarding_multilingual_secondary_text
+)
+
+val zeroPageImageViewCentered = listOf(
+    AppCompatImageView::class.java,
+    R.id.imageViewCentered,
+    R.drawable.w_nav_mark
+)
+
+val zeroPageLanguageList = listOf(
+    RecyclerView::class.java,
+    R.id.languageList,
+    null
+)
+
+
+
+
+
